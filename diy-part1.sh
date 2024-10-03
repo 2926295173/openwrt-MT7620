ff1499a81f3b4224c86f3f92c8a4dd438019093b
AOIAN <gaojian292629@gmail.com>
2024-10-03 18:06:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,9 +23,9 @@ popd
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-echo 'src-git luci-app-minieap https://github.com/BoringCat/luci-app-minieap' >>feeds.conf.default
+echo 'src-git luci-app-minieap https://github.com/BoringCat/luci-app-minieap.git' >>feeds.conf.default
 
-echo 'src-git minieap https://github.com/BoringCat/minieap-openwrt' >>feeds.conf.default
+echo 'src-git minieap https://github.com/BoringCat/minieap-openwrt.git' >>feeds.conf.default
 
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
