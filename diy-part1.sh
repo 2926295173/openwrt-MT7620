446392e61378719ebedbd9dc6a286e0924761e71
AOIAN <gaojian292629@gmail.com>
2024-10-03 15:15:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,16 +10,23 @@
 # See /LICENSE for more information.
 #
 
+
+git clone https://github.com/openwrt-dev/po2lmo.git
+pushd po2lmo
+make && sudo make install
+popd
+
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 echo 'src-git luci-app-minieap https://github.com/BoringCat/luci-app-minieap' >>feeds.conf.default
 
 echo 'src-git minieap https://github.com/BoringCat/minieap-openwrt' >>feeds.conf.default
 
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+
